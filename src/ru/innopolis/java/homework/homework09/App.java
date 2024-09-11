be0a8b1ada3b96ba0c37dbe9eec8f30e6a7a765f
ilya <Ilya-smirnov97@mail.ru>
2024-09-11 20:55:51 +0300

    Добавил homework 09.

--- src/ru/innopolis/java/homework/homework09/App.java
+++ src/ru/innopolis/java/homework/homework09/App.java
@@ -3,71 +3,91 @@ package ru.innopolis.java.homework.homework09;
 import ru.innopolis.java.homework.homework09.cars.Car;
 import ru.innopolis.java.homework.homework09.cars.PerformanceCar;
 import ru.innopolis.java.homework.homework09.cars.ShowCar;
-import ru.innopolis.java.homework.homework09.races.CasualRace;
-import ru.innopolis.java.homework.homework09.races.DragRace;
-import ru.innopolis.java.homework.homework09.races.DriftRace;
+import ru.innopolis.java.homework.homework09.races.*;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-        // Создание объектов Car, PerformanceCar, ShowCar
-        Car car1 = new Car("Toyota", "Camry", 2020, 200, 8, 5, 10);
-        Car car2 = new Car("Honda", "Civic", 2019, 180, 9, 6, 11);
-        PerformanceCar performanceCar1 = new PerformanceCar("Ferrari", "F8", 2021, 720, 3, 4, 9);
-        PerformanceCar performanceCar2 = new PerformanceCar("Porsche", "911", 2022, 650, 4, 5, 8);
-        ShowCar showCar1 = new ShowCar("Lamborghini", "Aventador", 2022, 770, 2, 4, 8, 5);
-        ShowCar showCar2 = new ShowCar("Bugatti", "Chiron", 2023, 1500, 1, 3, 10, 7);
+        // Чтение машин из файла
+        List<Car> cars = readCarsFromFile("C:\\Users\\ilyas\\Учеба\\Home_Work2\\src\\ru\\innopolis\\java\\homework\\homework09\\cars.txt");
 
         // Создание объектов гонок
         CasualRace casualRace = new CasualRace(500, "City Circuit", 10000);
-        casualRace.getParticipants().add(car1);
-        casualRace.getParticipants().add(car2);
-        casualRace.getParticipants().add(performanceCar1);
-        casualRace.getParticipants().add(performanceCar2);
-        casualRace.getParticipants().add(showCar1);
-        casualRace.getParticipants().add(showCar2);
-
         DragRace dragRace = new DragRace(400, "Highway", 15000);
-        dragRace.getParticipants().add(car1);
-        dragRace.getParticipants().add(car2);
-        dragRace.getParticipants().add(performanceCar1);
-        dragRace.getParticipants().add(performanceCar2);
-        dragRace.getParticipants().add(showCar1);
-        dragRace.getParticipants().add(showCar2);
-
         DriftRace driftRace = new DriftRace(300, "Mountain Road", 12000);
-        driftRace.getParticipants().add(car1);
-        driftRace.getParticipants().add(car2);
-        driftRace.getParticipants().add(performanceCar1);
-        driftRace.getParticipants().add(performanceCar2);
-        driftRace.getParticipants().add(showCar1);
-        driftRace.getParticipants().add(showCar2);
+        TimeLimitRace timeLimitRace = new TimeLimitRace(600, "Desert Road", 20000, 300);
+        CircuitRace circuitRace = new CircuitRace(700, "Race Track", 25000, 5);
 
-        // Создание объекта Garage и добавление машин
-        Garage garage = new Garage();
-        garage.addCar(car1);
-        garage.addCar(car2);
-        garage.addCar(performanceCar1);
-        garage.addCar(performanceCar2);
-        garage.addCar(showCar1);
-        garage.addCar(showCar2);
-
-        // Модификация машин в гараже
-        garage.modifyCar(car1, 250, 7, 6, 12);
-        garage.modifyCar(car2, 200, 8, 7, 13);
-        garage.modifyCar(performanceCar1, 800, 2, 5, 10);
-        garage.modifyCar(performanceCar2, 750, 3, 6, 9);
-        garage.modifyCar(showCar1, 800, 2, 5, 10);
-        garage.modifyCar(showCar2, 1550, 1, 4, 12);
+        // Добавление участников
+        for (Car car : cars) {
+            casualRace.getParticipants().add(car);
+            dragRace.getParticipants().add(car);
+            driftRace.getParticipants().add(car);
+            timeLimitRace.getParticipants().add(car);
+            circuitRace.getParticipants().add(car);
+        }
 
         // Определение победителей гонок
         Car casualRaceWinner = casualRace.determineWinner();
         Car dragRaceWinner = dragRace.determineWinner();
         Car driftRaceWinner = driftRace.determineWinner();
+        Car timeLimitRaceWinner = timeLimitRace.determineWinner();
+        Car circuitRaceWinner = circuitRace.determineWinner();
 
-        // Вывод информации
-        System.out.println("Casual Race Winner: " + casualRaceWinner);
-        System.out.println("Drag Race Winner: " + dragRaceWinner);
-        System.out.println("Drift Race Winner: " + driftRaceWinner);
-        System.out.println(garage);
+        // Формирование результатов
+        List<String> results = new ArrayList<>();
+        results.add("На гонке: CasualRace\nПобеду одержала машина: " + casualRaceWinner);
+        results.add("На гонке: DragRace\nПобеду одержала машина: " + dragRaceWinner);
+        results.add("На гонке: DriftRace\nПобеду одержала машина: " + driftRaceWinner);
+        results.add("На гонке: TimeLimitRace\nПобеду одержала машина: " + timeLimitRaceWinner);
+        results.add("На гонке: CircuitRace\nПобеду одержала машина: " + circuitRaceWinner);
+
+        // Запись результатов в файл
+        writeResultsToFile("C:\\Users\\ilyas\\Учеба\\Home_Work2\\src\\ru\\innopolis\\java\\homework\\homework09\\race_results.txt", results);
+    }
+
+    private static List<Car> readCarsFromFile(String fileName) {
+        List<Car> cars = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(",");
+                if (parts.length == 7) {
+                    cars.add(new Car(parts[0], parts[1], Integer.parseInt(parts[2]),
+                            Integer.parseInt(parts[3]), Integer.parseInt(parts[4]),
+                            Integer.parseInt(parts[5]), Integer.parseInt(parts[6])));
+                } else if (parts.length == 8 && parts[7].equals("Performance")) {
+                    cars.add(new PerformanceCar(parts[0], parts[1], Integer.parseInt(parts[2]),
+                            Integer.parseInt(parts[3]), Integer.parseInt(parts[4]),
+                            Integer.parseInt(parts[5]), Integer.parseInt(parts[6])));
+                } else if (parts.length == 9 && parts[8].equals("Show")) {
+                    cars.add(new ShowCar(parts[0], parts[1], Integer.parseInt(parts[2]),
+                            Integer.parseInt(parts[3]), Integer.parseInt(parts[4]),
+                            Integer.parseInt(parts[5]), Integer.parseInt(parts[6]), Integer.parseInt(parts[7])));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return cars;
+    }
+
+    private static void writeResultsToFile(String fileName, List<String> results) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(fileName))) {
+            for (String result : results) {
+                bw.write(result);
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
+
