6035bfcc6dee9d8578c412b8d16c6e4109d01987
ilya <Ilya-smirnov97@mail.ru>
2024-09-23 22:18:21 +0300

    Добавил  attestation02.

---
+++ src/ru/innopolis/java/homework/homework011/Car.java
@@ -0,0 +1,67 @@
+package ru.innopolis.java.homework.homework011;
+
+public class Car {
+    // Поля класса
+    private String number;
+    private String model;
+    private String color;
+    private int mileage;
+    private int cost;
+
+    // Конструктор класса
+    public Car(String number, String model, String color, int mileage, int cost) {
+        this.number = number;
+        this.model = model;
+        this.color = color;
+        this.mileage = mileage;
+        this.cost = cost;
+    }
+
+    // Геттеры и сеттеры
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(int mileage) {
+        this.mileage = mileage;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    // Переопределение метода toString для удобного вывода информации об автомобиле
+    @Override
+    public String toString() {
+        return String.format("Number: %s, Model: %s, Color: %s, Mileage: %d, Cost: %d", number, model, color, mileage, cost);
+    }
+}
+
